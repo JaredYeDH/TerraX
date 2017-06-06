8f092798ea4467140ef66c422ea806d1c387bef3
ottox <xiatuo.otto@gmail.com>
2017-06-06 19:15:39 +0800

    login map

--- server/login_server/login_account/login_account_manager.h
+++ server/login_server/login_account/login_account_manager.h
@@ -11,9 +11,11 @@ namespace terra
 	{
 		MAKE_INSTANCE(LoginAccountManager);
 		DISABLE_COPY(LoginAccountManager);
-		using LoginAccountMap = std::unordered_map<std::string, std::unique_ptr<LoginAccount> >;
+		using LoginAccountMap = std::unordered_map<int, std::unique_ptr<LoginAccount> >;
+		using Account2FdMap = std::unordered_map<std::string, int >;
 	private:
 		LoginAccountMap account_map_;
+		Account2FdMap account2fd_map_;
 	public:
 		LoginAccountManager();
 		~LoginAccountManager() = default;
